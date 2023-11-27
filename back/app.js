7ba17600c027deb5deb70f98539765b2b2216cdd
Guillaume Pontanier <guillaume.pontanier@gmail.com>
2023-11-27 11:21:52 +0100

    Modification du fichier app.js

--- back/app.js
+++ back/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const productRoutes = require('./routes/product');
+const campaignRoutes = require('./routes/campaign');
 
 const app = express();
 
@@ -18,6 +18,6 @@ app.use(express.static('images'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use('/api/products', productRoutes);
+app.use('/api/campaigns', campaignRoutes);
 
 module.exports = app;
