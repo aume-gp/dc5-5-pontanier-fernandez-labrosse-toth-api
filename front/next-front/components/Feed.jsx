de4b7de60c2ac4860f35475d8d867729adbceae1
GabrielToth <gabt71@gmail.com>
2023-11-28 11:23:10 +0100

    update Feed component

--- front/next-front/components/Feed.jsx
+++ front/next-front/components/Feed.jsx
@@ -1,47 +1,46 @@
-
 "use client";
 
 import { useState, useEffect } from "react";
 
-const Feed = ({data}) => {
+const Feed = ({ data }) => {
+  const [allCampaigns, SetAllCampaigns] = useState([]);
 
-    const [allCampaigns, SetAllCampaigns] = useState([]);
+  const FetchCampaigns = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/api/campaigns`);
+      const data = await response.json();
+      console.log(data);
+      SetAllCampaigns(data);
+    } catch (error) {
+      console.error("Error :", error);
+    }
+  };
 
-    const FetchCampaigns = async () => {
-        try {
-            const response = await fetch(`http://localhost:3001/api/campaigns/`);
-            const data = await response.json();
-            console.log(data);
-          
-        } catch (error) {
-          console.error("Error :", error);
-        }
-      };
-    
-      useEffect(() => {
-        FetchCampaigns();
-      }, []);
+  useEffect(() => {
+    FetchCampaigns();
+  }, []);
 
+  return (
+    <>
+      <section className="w-full flex justify-center">
+        <div className="container">
+          <div className="titles">
+            <h1>Nos produits</h1>
+            <h2>Une gamme d'articles exclusifs</h2>
+          </div>
 
-
-  return ( 
-    <> 
-    <div class="titles">
-    <h1>Nos produits</h1>
-    <h2>Une gamme d'articles exclusifs</h2>
-    </div>
-
-    <div className="flex flex-wrap">
-      {/* {data.map((campaigns) => (
+          <div className="flex flex-wrap">
+            {/* {data.map((campaigns) => (
         <Card
           key={campaigns._id}
           campaign={campaigns}
         />
       ))} */}
-    </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-
-export default Feed
+export default Feed;
