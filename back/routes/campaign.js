02da9999b017873691b2309adf4ce832416302ef
Guillaume Pontanier <guillaume.pontanier@gmail.com>
2023-11-28 10:22:12 +0100

    Mise à jour des bonnes routes

--- back/routes/campaign.js
+++ back/routes/campaign.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const campaignCtrl = require('../controllers/campaign');
 
 router.get('/', campaignCtrl.getAllCampaigns);
+router.get('/:id', campaignCtrl.getCampaignById);
+router.post('/', campaignCtrl.createCampaign);
+router.delete('/:id', campaignCtrl.deleteCampaign);
 
 module.exports = router;
